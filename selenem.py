ad0c206ea4c6c953dbc6fa10a5bf30f430f8a74b
sanjar <sanjar@>
2024-03-26 13:12:35 +0500

    add env

--- selenem.py
+++ selenem.py
@@ -1,11 +1,14 @@
-import time
 from unicodedata import category
-from bs4 import BeautifulSoup as bs
-import requests
+from dotenv import load_dotenv
+import os
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 
-BASE_URL = "https://portal.tafs.com/"
+load_dotenv()
+
+BASE_URL = os.getenv('TAFS_URL')
+USERNAME = os.getenv('TAFS_USER')
+PASSWORD = os.getenv('TAFS_PASS')
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
@@ -22,7 +25,7 @@ browser.get(BASE_URL)
 username = browser.find_element(by=By.ID, value="txt_username")
 password = browser.find_element(by=By.ID, value="txt_password")
 
-username.send_keys("200970768")
-password.send_keys("0768PUNT")
+username.send_keys(USERNAME)
+password.send_keys(PASSWORD)
 
 browser.find_element(value="btn_login", by=By.ID).click()
